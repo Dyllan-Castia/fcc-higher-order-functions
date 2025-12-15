9d7a3810f1356303a4d49a8e13f2f03e60fe321a
Dyllan Castia <castiadyllan@gmail.com>
2025-12-14 19:50:11 -0800

    Simplify index finding in getIndexToIns function
    Refactor index finding logic to use findIndex directly.

--- indexToIns.js
+++ indexToIns.js
@@ -1,7 +1,10 @@
+//Sorts the array, finds the index where the number should be inserted and returns that index.
 function getIndexToIns(arr, num) {
   arr.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length; i++) {
-   arr.findIndex(num => num > arr[i]);
-  }
+  
+  const index = arr.findIndex(value => num <= value);
+
+  return index === -1 ? arr.length : index;
 }
 console.log(getIndexToIns([10, 20, 30, 40], 35));
+//3
